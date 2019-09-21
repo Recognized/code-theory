7fdb3e6dddcf6b10d8aac96cd1b2220af8f95026
vladislav.saifulin <vladislav.saifulin@jetbrains.com>
2019-09-21 05:32:59 +0300

    Done

---
+++ README.md
@@ -0,0 +1,3 @@
+./gradlew runExample
+
+code: https://github.com/Recognized/code-theory/blob/master/src/main/kotlin/Main.kt
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,8 @@
 @file:JvmName("Main")
 
-import org.apache.commons.math3.special.Erf
-import kotlin.math.log10
-import kotlin.math.log2
+import org.apache.commons.math3.distribution.NormalDistribution
+import kotlin.math.pow
 import kotlin.math.sqrt
-import kotlin.random.Random
 
 data class Matrix(val source: List<List<Int>>) {
     fun height() = source.size
@@ -33,6 +31,8 @@ data class Vector(val source: List<Int>) {
 
     override fun toString(): String = source.joinToString(prefix = "[", postfix = "]") { it.toString() }
 }
+
+data class VectorDouble(val source: List<Double>)
 
 operator fun Vector.times(matrix: Matrix): Vector {
     if (matrix.height() != length()) error("Invalid operation vector${length()} * matrix(${matrix.height()} x ${matrix.width()})")
@@ -73,14 +73,22 @@ const val SPEED = 0.6
 const val DEFAULT_COUNT = 1000
 
 val CODES = allVectorsSize(6).associateBy { it * G }
+val ALL_VECTORS = allVectorsSize(6)
 
-infix fun Vector.hammingTo(other: Vector): Int {
-    if (length() != other.length()) error("Error 15") // lol
-    return source.zip(other.source).sumBy { (a, b) -> 0.takeIf { a == b } ?: 1 }
+infix fun Vector.closestFromByHamming(vectors: Collection<Vector>): Vector {
+    infix fun Vector.hammingTo(other: Vector): Int {
+        if (length() != other.length()) error("Error 15") // lol
+        return source.zip(other.source).sumBy { (a, b) -> 0.takeIf { a == b } ?: 1 }
+    }
+    return vectors.minBy { it hammingTo this }!!
 }
 
-infix fun Vector.closestFrom(vectors: Collection<Vector>): Vector {
-    return vectors.minBy { it hammingTo this }!!
+infix fun VectorDouble.closestFrom(vectors: Collection<Vector>): Vector {
+    return vectors.maxBy { this * it }!!
+}
+
+operator fun VectorDouble.times(vector: Vector): Double {
+    return source.zip(vector.source).sumByDouble { (a, b) -> a * b }
 }
 
 operator fun Matrix.times(other: Matrix): Matrix {
@@ -96,15 +104,32 @@ operator fun Matrix.times(other: Matrix): Matrix {
     )
 }
 
-infix fun Vector.withError(probability: Double): Vector {
-    return Vector(source.map { it.takeIf { Random.nextDouble() >= probability } ?: (it + 1) % 2 })
+infix fun Vector.withNoise(distribution: NormalDistribution): VectorDouble {
+    return VectorDouble(source.map { 2 * it - 1 + distribution.sample() })
 }
 
-fun errorProbability(modulationLevels: Int, noise: Double, speed: Double): Double {
-    val error = bitErrorRate(noise, modulationLevels, speed)
-    return allVectorsSize(6).map { source ->
+infix fun Vector.withHardNoise(distribution: NormalDistribution): Vector {
+    return Vector(source.map { if (2 * it - 1 + distribution.sample() < 0) 0 else 1 })
+}
+
+fun hardErrorProbability(noise: NormalDistribution): Double {
+    return ALL_VECTORS.map { source ->
         (0 until DEFAULT_COUNT).map {
-            val received = (source * G) withError error
+            val received = (source * G) withHardNoise noise
+            val decoded = CODES[received closestFromByHamming CODES.keys]
+            if (decoded != source) {
+                1
+            } else {
+                0
+            }
+        }.average()
+    }.average()
+}
+
+fun softErrorProbability(noise: NormalDistribution): Double {
+    return ALL_VECTORS.map { source ->
+        (0 until DEFAULT_COUNT).map {
+            val received = (source * G) withNoise noise
             val decoded = CODES[(received closestFrom CODES.keys)]
             if (decoded != source) {
                 1
@@ -115,25 +140,16 @@ fun errorProbability(modulationLevels: Int, noise: Double, speed: Double): Doubl
     }.average()
 }
 
-fun rawErrorProbability(modulationLevels: Int, noise: Double): Double {
-    val error = bitErrorRate(noise, modulationLevels, 1.0)
-    return allVectorsSize(6).map { source ->
+fun rawErrorProbability(noise: NormalDistribution): Double {
+    return ALL_VECTORS.map { source ->
         (0 until DEFAULT_COUNT).map {
-            if ((source withError error) != source) {
+            if (((source withNoise noise) closestFrom ALL_VECTORS) != source) {
                 1
             } else {
                 0
             }
         }.average()
     }.average()
-}
-
-// https://old.telesputnik.ru/archive/pdf/181/70.pdf
-// approximation, error ~ 0.2db
-fun bitErrorRate(noise: Double, modulationLevels: Int, speed: Double): Double {
-    val energy = noise - 10 * log10(log2(modulationLevels.toDouble() / speed))
-    return 2 * (1 - 1 / sqrt(modulationLevels.toDouble())) *
-            Erf.erfc(sqrt(3 * log2(modulationLevels.toDouble()) * energy / (2 * (modulationLevels - 1)))) / energy
 }
 
 fun binarySearch(left: Double, right: Double, choose: (Double) -> Boolean): Double {
@@ -150,28 +166,32 @@ fun binarySearch(left: Double, right: Double, choose: (Double) -> Boolean): Doub
     return l
 }
 
+fun sigma(noise: Double): Double = sqrt(0.5 * 10.0.pow(-noise / 10.0) / SPEED)
+
 fun main() {
-    listOf(2, 4, 8, 16, 64).forEach br@{ levels ->
-        println()
-        (1..100).step(1).forEach { noise ->
-            if (!bitErrorRate(noise.toDouble(), levels, SPEED).isNaN()) {
-                val error = errorProbability(levels, noise.toDouble(), SPEED)
-                println("[${levels}QAM] signal/noise=${noise}dB, \terror=$error")
-                if (error < 1e-10) return@br
-            }
-        }
+    (1..30).step(1).forEach { noise ->
+        val error = softErrorProbability(NormalDistribution(0.0, sigma(noise.toDouble())))
+        println("[SOFT] Eb/N0=${noise}dB, \terror=$error")
     }
 
-    listOf(2, 4, 8, 16, 64).forEach { levels ->
-        val raw = binarySearch(0.0, 100.0) {
-            if (it.isNaN()) return@binarySearch false
-            rawErrorProbability(levels, it) < 1e-5
-        }
-        val cool = binarySearch(0.0, 100.0) {
-            if (it.isNaN()) return@binarySearch false
-            errorProbability(levels, it, SPEED) < 1e-5
-        }
-        println("[${levels}QAM] p=${1e-5}, efficiency(S'/N' - S/N)=${raw - cool}")
+    (1..30).step(1).forEach { noise ->
+        val error = hardErrorProbability(NormalDistribution(0.0, sigma(noise.toDouble())))
+        println("[HARD] Eb/N0=${noise}dB, \terror=$error")
     }
+
+    println("P=${1e-5}")
+
+    val raw = binarySearch(0.0, 100.0) {
+        rawErrorProbability(NormalDistribution(0.0, sigma(it))) < 1e-5
+    }
+    println("[NO DECODING]: Eb/N0 = $raw")
+    val hard = binarySearch(0.0, 100.0) {
+        hardErrorProbability(NormalDistribution(0.0, sigma(it))) < 1e-5
+    }
+    println("[HARD]: Eb/N0 = $hard, efficiency = ${raw - hard}")
+    val cool = binarySearch(0.0, 100.0) {
+        softErrorProbability(NormalDistribution(0.0, sigma(it))) < 1e-5
+    }
+    println("[SOFT]: Eb/N0 = $raw, efficiency = ${raw - cool}")
 
 }
